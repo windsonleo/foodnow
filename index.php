79bffcd0d4bb49e647ede3946701bc28672f25f9
windsonleo <windsor_melo@hotmail.com>
2023-02-18 13:05:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ $_SESSION['existecliente']=$existecliente;
 
 
 
-if(isset($_SESSION['carrinho'])) {
+/*if(isset($_SESSION['carrinho'])) {
 
 
 
@@ -162,7 +162,139 @@ if(isset($_SESSION['carrinho'])) {
  
      $_SESSION['endereco']=$endereco;
  
+}*/
+ 
+ 
+ 
+ 
+ if(isset($_SESSION['carrinho'])) {
+
+
+
+     $carrinho = $_SESSION['carrinho'];
+     $totalitenscarrinho = $_SESSION['totalitem'];
+     $totalvalorcarrinho = $_SESSION['totalvalor'];
+
+
+
+  } else {
+
+
+      $carrinho =  new Carrinho();
+    
+     $totalitenscarrinho = 0 ;
+
+     $totalvalorcarrinho = 0.00;
+
+
+       $_SESSION['carrinho'] =  $carrinho;
+
+
+       $_SESSION['totalvalor'] = $totalvalorcarrinho;
+
+       $_SESSION['totalitem'] = $totalitenscarrinho ;
+
+
+
+
+  }
+
+
+    if(isset($_SESSION['tel'])){
+
+    $tel = $_SESSION['tel'];
+
+   // echo 'telefone setado : ' .$tel ;
+
+
+  }else {
+
+    $tel ='81-98833-0011';
+    $_SESSION['tel']=$tel;
+  //  echo 'telefone NAO setado : ' ;
+
+
+  }
+
+
+
+  if(isset($_SESSION['cliente'])){
+
+    $cliente = $_SESSION['cliente'];
+
+  //  echo 'cliente setado : ' .$cliente->getnome();
+
+
+  }else {
+
+    $cliente = new Cliente();
+    $id=22;
+    $nomecli='padrão';
+    $cliente->setid($id);
+    $cliente->setnome($nomecli);
+    $_SESSION['cliente']=$cliente;
+  //  echo 'cliente NAO setado : ' ;
+
+
+  }
+
+
+    if(isset($_SESSION['endereco'])){
+
+    $endereco = $_SESSION['endereco'];
+
+   // echo 'endereco setado : ' .$endereco ;
+
+    $temendereco = $_SESSION['temendereco'];
+    // $carrinho->setcliente($cliente);
+     $endereco_id = $_SESSION['endereco_id'];
+     $cliente->setendereco($endereco);
+
+
+  }else {
+
+    $endereco = new Endereco();
+    $id=33;
+    $log='rua da fantasia';
+    $cid='Recife';
+    $num='288';
+    $cepaux='54420-450';
+
+    $endereco->setid($id);
+    $endereco->setlogradouro($log);
+    $endereco->setcidade($cid);
+    $endereco->setnumero($num);
+    $endereco->setcep($cepaux);
+
+    $cliente->setendereco($endereco);
+
+
+
+    $_SESSION['endereco']=$endereco;
+    $_SESSION['endereco_id'] = 0;
+   // echo 'endereco NAO setado : ' ;
+      $temendereco = false;
+
+      $_SESSION['temendereco'] = $temendereco;
+
+
+  }
+
+
+      if(isset($_SESSION['usuario_nome'])){
+
+        $usuario_nome = $_SESSION['usuario_nome'];
+
+
+  }else {
+
+
+    $_SESSION['usuario_nome']='usu padrao';
+
+
 }
+ 
+ 
 
 ?>
 

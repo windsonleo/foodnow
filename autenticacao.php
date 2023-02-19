3d60126c17a1f35cf10fd20c6fa39a6b4abe4dea
windsonleo <windsor_melo@hotmail.com>
2023-02-19 02:35:42 -0300

    Update autenticacao.php

--- autenticacao.php
+++ autenticacao.php
@@ -34,20 +34,12 @@ $senha = $_POST["senha"];
 ConsultarUsuario($nome,$senha,$conn);
 
 
- function ConsultarUsuario($nome,$senha,$conexao){
+ function ConsultarUsuario($nome,$senha,$conexao) {
   $conn = $conexao;
 
 $sql = "SELECT * FROM usuario WHERE usuario.nome= '$nome' AND usuario.senha='$senha' ";
 
-/*if (mysqli_query($conn, $sql)) {
-  echo "Registro usuario Consultado com sucesso";
-  mysqli_close($conn);
-   header("Location: http://cardapio.com/adm.php");
-   exit();
 
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}*/
      
      $sqlcliente = pg_query($conn,"SELECT * FROM usuario WHERE usuario.nome= '$nome' AND usuario.senha='$senha' ") or die("Erro");
 while($dadoscli = pg_fetch_assoc($sqlcliente)){
@@ -72,36 +64,9 @@ if($row == 1) {
 
 }
 
+     pg_close($conn);
  }
 
-/* function ListarUsuarios($conexao){
-  $conn = $conexao;
-
-$sql = "SELECT * FROM usuario";
-
-if (mysqli_query($conn, $sql)) {
-  echo "Registros listados usuario  com sucesso";
 
 
-} else {
-  echo "Error ao listar: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-
-
-}*/
-
-/*
-INSERT INTO `paciente` (`id`, `ativo`, `nome`, `datanascimento`, `foto`, `email`, `telefone`, `psicologo_id`, `dataregistro`) VALUES (NULL, '1', 'julia maria', '2022-09-21 12:59:57.000000', 'julia.png', 'julia@hotmail.com', '87-98769-6455', '1', CURRENT_TIMESTAMP)*/
-
-/*$sql2 = "INSERT INTO psicologo (id, ativo, nome, datanascimento, foto, email, telefone,dataregistro)
-VALUES (NULL,'1', 'WILL','1982-03-26 09:10:07.000000','WILL.jpg' , 'WILL@example.com','8198777-5544',CURRENT_TIMESTAMP)";
-
-if (mysqli_query($conn, $sql2)) {
-  echo "Registro psicologo incluido com sucesso";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}*/
-
-pg_close($conn);
 ?>

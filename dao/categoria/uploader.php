9493147dec1f4c4c42c0e299c0a11508f2d2ee86
windsonleo <windsor_melo@hotmail.com>
2023-02-20 09:57:33 -0300

    Update uploader.php

--- dao/categoria/uploader.php
+++ dao/categoria/uploader.php
@@ -1,6 +1,6 @@
 <?php
    // include('db.php');
-    $pasta = "/images/categoria/";
+    $pasta = "../../images/categoria/";
 
   /* formatos de imagem permitidos */
     $permitidos = array(".jpg",".jpeg",".gif",".png", ".bmp");
